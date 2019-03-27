7878e2ec63bf642529ef99579d5caaf0f656a6dc
ਅਕਸ਼ੇ ਗੋਸਵਾਮੀ <akshay.deep0@gmail.com>
2019-03-27 11:50:47 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+const root = "D:\\audio-books\\rel\\maskeen_ji_katha"
+const sp = os.PathSeparator
+
+func main() {
+	files, err := ioutil.ReadDir(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		dir := root + string(sp) + file.Name()
+		size := int64(0)
+
+		filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err == nil && !info.IsDir() {
+				size = size + info.Size()
+			}
+
+			return nil
+		})
+
+		fmt.Println(file.Name(), size/(1024*1024), "mb")
+	}
+}
